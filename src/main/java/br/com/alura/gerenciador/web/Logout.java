0e5b8f4070a2650b4195512c86346307b2b0b87e
Adriano Marinho <adrianomarinhomelo9@gmail.com>
2017-01-31 22:42:07 -0200

    Aula 1-7 - Curso de Servelet Alura

---
+++ src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -0,0 +1,25 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns="/logout")
+public class Logout extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		session.removeAttribute("usuario.logado");
+				
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><body>Logout efetuado.</body></html>");
+	}
+}
