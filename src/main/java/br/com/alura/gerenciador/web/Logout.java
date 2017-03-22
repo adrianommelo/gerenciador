b57985c4e2bdbba1ca501bb0877a35dd6cffe783
Adriano Marinho <Adriano.Melo@compsisnet.com.br>
2017-03-22 11:12:06 -0300

    Modificado logica de logout e implementado redirect da page de logout no servidor

--- src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -1,8 +1,8 @@
 package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,8 +18,7 @@ public class Logout extends HttpServlet {
 		
 		HttpSession session = req.getSession();
 		session.removeAttribute("usuario.logado");
-				
-		PrintWriter writer = resp.getWriter();
-		writer.println("<html><body>Logout efetuado.</body></html>");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logout.html");
+	    dispatcher.forward(req, resp);
 	}
 }
